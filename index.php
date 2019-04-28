1ebce3a0213174127187b6d697e3d7789c85ed49
JRugerio <50065802+JRugerio@users.noreply.github.com>
2019-04-27 23:31:59 -0500

    Create index.php

---
+++ index.php
@@ -0,0 +1,61 @@
+<!DOCTYPE html PUBLIC "-//W3C//DTD XHTML 1.0 Strict//EN" "http://www.w3.org/TR/xhtml1/DTD/xhtml1-strict.dtd">
+<html xmlns="http://www.w3.org/1999/xhtml">
+<head>
+<title>Portafolio</title>
+<meta http-equiv="Content-Language" content="English" />
+<meta http-equiv="Content-Type" content="text/html; charset=UTF-8" />
+<link rel="stylesheet" type="text/css" href="style.css" media="screen" />
+</head>
+<body>
+
+<div id="wrap">
+
+<div id="header">
+<h1><a href="#">Portafolio</a></h1>
+<h2>Facultad de Ciencias de la computación </h2>
+<h2>Aplicaciones multimedia</h2>
+</div>
+
+<div id="top"> </div>
+<div id="menu">
+<ul>
+<li><a href="index.php">Inicio</a></li>
+<li><a href="registro.php">Registro</a></li>
+<li><a href="acerca.php">Acerca de</a></li>
+</ul>
+</div>
+
+<div id="content">
+<div class="left"> 
+
+<h2>Bienvenido a la plataforma de gestion de portafolios </h2>
+<div class="articles"> Da a conocer tu trabajo gracias a nuestro contacto con empresas nacionales <br />
+    <br />
+    <br />
+      <br />
+</div>
+</div>
+
+<div class="right"> 
+<form action="validar.php" method="POST">
+<h2>Login:</h2>
+<ul>
+<li><input type="text" name="nombre" size="15" required /></li>
+</ul>
+
+<h2>Password:</h2>
+<ul>
+<li><input type="password" name="passwd" size ="15" required /></li>
+</ul>
+<input type="submit" name="enviar" value="Accesar"/>
+</form>
+</div>
+<div style="clear: both;"> </div>
+</div>
+
+<div id="bottom"> </div>
+<div id="footer"></div>
+</div>
+
+</body>
+</html>
